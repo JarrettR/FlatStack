cba207e4bba064eaae5badaa658e1f223b6a84ac
JarrettR <jrainier@gmail.com>
2019-09-12 21:01:30 -0700

    Started setting up constraint systems

--- solver.py
+++ solver.py
@@ -1,15 +1,26 @@
 import kiwisolver
 
+fudge_factor = 0.01
 
 class Solver(object):
     def __init__(self, joints, layers):
         self.joints = joints
         self.layers = layers
+        self.solver = kiwisolver.Solver()
         
     def solve(self):
         joints = self.joints
         layers = self.layers
-        #joint_assoc = {}
+        
+        
+        for l in layers:
+            l = self.define_shape(l)
+            
+            if l.fixed == True:
+                self.add_fixed(l)
+                
+        print(self.solver.dumps())
+        
         print(joints)
         for jointlayer in joints:
             print(jointlayer)
@@ -23,6 +34,52 @@ class Solver(object):
                             #joint_assoc[jointlayer].append(l.name)
         #print(joint_assoc)
         return layers
+        
+    #Todo: only constrain first point to global coordinate
+    #Or: constrain bounding box to also fix rotation
+    def add_fixed(self, layer):
+        i = 0
+        
+        for p in layer.straight_pairs:
+            #path4581_p1_x
+            name = layer.name + '_p' + str(i) + '_'
+            nameX = name + 'x'
+            nameY = name + 'y'
+            x = kiwisolver.Variable(nameX)
+            y = kiwisolver.Variable(nameY)
+            self.solver.addConstraint((x == p[0]) | "strong")
+            self.solver.addConstraint((y == p[1]) | "strong")
+            i += 1
+        return layer
+            
+    # Give points relative constraints to previous point 
+    def define_shape(self, layer):
+        i = 0
+        x_p = 0
+        y_p = 0
+        xp_p = 0.0
+        yp_p = 0.0
+        
+        for p in layer.straight_pairs:
+            #path4581_p1_x
+            name = layer.name + '_p' + str(i) + '_'
+            nameX = name + 'x'
+            nameY = name + 'y'
+            x = kiwisolver.Variable(nameX)
+            y = kiwisolver.Variable(nameY)
+            xp = p[0]
+            yp = p[1]
+            if i > 0:
+                x_constraint = (x == (x_p + (xp - xp_p)))
+                y_constraint = (y == (y_p + (yp - yp_p)))
+                self.solver.addConstraint(x_constraint | "strong")
+                self.solver.addConstraint(y_constraint | "strong")
+                x_p = x
+                y_p = y
+                xp_p = xp
+                yp_p = yp
+            i += 1
+        return layer
 
         
     #Naive bounding box implementation
