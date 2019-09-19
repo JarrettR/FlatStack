2335ba0ff9bb9b82c1731ef21dddb8636206fb18
JarrettR <jrainier@gmail.com>
2019-09-18 20:34:09 -0700

    Added joint constraints

--- solver.py
+++ solver.py
@@ -1,12 +1,16 @@
 import kiwisolver
 
 fudge_factor = 0.01
+strength_define = "strong"
+strength_fixed = "strong"
+strength_joint = "medium"
 
 class Solver(object):
     def __init__(self, joints, layers):
         self.joints = joints
         self.layers = layers
         self.solver = kiwisolver.Solver()
+        self.variables = {}
         
     def solve(self):
         joints = self.joints
@@ -14,12 +18,12 @@ class Solver(object):
         
         
         for l in layers:
+            #Todo: check that shape actually needs constraints before defining
             l = self.define_shape(l)
             
+            #Todo: Finish / test
             if l.fixed == True:
                 self.add_fixed(l)
-                
-        print(self.solver.dumps())
         
         print(joints)
         for jointlayer in joints:
@@ -28,13 +32,46 @@ class Solver(object):
                 for l in layers:
                     print(l.name)
                     #print(l)
+                    i = 0
                     for p in l.straight_pairs:
                         if(self.encloses(p, jp)):
-                            print(jointlayer, l.name)
+                            print(jointlayer, l.name, i, p)
+                            self.add_joint(jointlayer, l.name, i, p)
                             #joint_assoc[jointlayer].append(l.name)
+                        i += 1
         #print(joint_assoc)
+                
+        print(self.solver.dumps())
         return layers
         
+    def add_joint(self, jname, lname, pname, p):
+
+        #path4581_p1_x
+        layername = lname + '_p' + str(pname) + '_'
+        layernameX = layername + 'x'
+        layernameY = layername + 'y'
+        # lx = kiwisolver.Variable(layernameX)
+        # ly = kiwisolver.Variable(layernameY)
+        lx = self.variables[layernameX]
+        ly = self.variables[layernameY]
+        
+        jointname = 'joint_' + str(jname) + '_'
+        jointnameX = jointname + 'x'
+        jointnameY = jointname + 'y'
+        if jointnameX not in self.variables.keys():
+            jx = kiwisolver.Variable(jointnameX)
+            jy = kiwisolver.Variable(jointnameY)
+            self.variables[jointnameX] = jx
+            self.variables[jointnameY] = jy
+        else:
+            jx = self.variables[jointnameX]
+            jy = self.variables[jointnameY]
+        
+        self.solver.addConstraint((lx == jx) | strength_joint)
+        self.solver.addConstraint((ly == jy) | strength_joint)
+            
+            
+            
     #Todo: only constrain first point to global coordinate
     #Or: constrain bounding box to also fix rotation
     def add_fixed(self, layer):
@@ -47,8 +84,8 @@ class Solver(object):
             nameY = name + 'y'
             x = kiwisolver.Variable(nameX)
             y = kiwisolver.Variable(nameY)
-            self.solver.addConstraint((x == p[0]) | "strong")
-            self.solver.addConstraint((y == p[1]) | "strong")
+            self.solver.addConstraint((x == p[0]) | strength_define)
+            self.solver.addConstraint((y == p[1]) | strength_define)
             i += 1
         return layer
             
@@ -67,6 +104,8 @@ class Solver(object):
             nameY = name + 'y'
             x = kiwisolver.Variable(nameX)
             y = kiwisolver.Variable(nameY)
+            self.variables[nameX] = x
+            self.variables[nameY] = y
             xp = p[0]
             yp = p[1]
             if i > 0:
