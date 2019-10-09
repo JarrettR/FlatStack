1bf24974b98378766e2026ba38013203e197bc63
JarrettR <jrainier@gmail.com>
2019-10-08 20:43:54 -0700

    Vector  transformation utility functions

--- solver.py
+++ solver.py
@@ -12,9 +12,55 @@ class Solver(object):
         self.joints = joints
         self.layers = layers
         
-    def solve(self):
+    def solve(self, solveJointless = False):
         joints = self.joints
         layers = self.layers
         
+        for layer in layers:
+            if solveJointless == False and layer.joint == False:
+                print("Solving for ", layer.id)
+                self.solve_layer(layer)
+            elif layer.joint == True:
+                print("Solving for ", layer.id)
+        
         return layers
         
+    def solve_layer(self, layer):
+        origin = self.find_origin(layer)
+        translate_x = layer.translate.x
+        translate_y = layer.translate.y
+        translate_z = layer.translate.z
+        for i, point in enumerate(layer.straight_pairs):
+            print(point)
+            x = (point[0] - origin[0]) + translate_x
+            y = (point[1] - origin[1]) + translate_y
+            z =  translate_z
+            
+    def point_transform(self, x, y, z, axis):
+        #Transform from rotation axis 
+    
+    def find_origin(self, layer):
+        max_x = 0
+        max_y = 0
+        min_x = 0
+        min_y = 0
+        first_run = True
+        for point in layer.straight_pairs:
+            if first_run == True:
+                max_x = point[0]
+                max_y = point[1]
+                min_x = point[0]
+                min_y = point[1]
+                first_run = False
+            else:
+                if point[0] > max_x:
+                    max_x = point[0]
+                elif point[0] < min_x:
+                    min_x = point[0]
+                    
+                if point[1] > max_y:
+                    max_y = point[1]
+                elif point[1] < min_y:
+                    min_y = point[1]
+            print(point)
+        return [(max_x + min_x) / 2, (max_y + min_y) / 2]
